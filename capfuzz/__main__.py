2f422d42aec52b4f087b0b4276f56e4e5a43fe9a
Ajin Abraham <ajin25@gmail.com>
2018-03-11 17:07:36 +0530

    class like

--- capfuzz/__main__.py
+++ capfuzz/__main__.py
@@ -33,49 +33,71 @@ import capfuzz.settings as settings
 APPSERVER = None
 WEBSERVER = None
 
+class CapFuzz:
+    def __init__(self):
+        self.kill = self.signal_handler
 
-def signal_handler(*args, **kwargs):
-    try:
-        APPSERVER.shutdown()
-    except:
-        pass
-    try:
-        tornado.ioloop.IOLoop.instance().stop()
-    except:
-        pass
-    sys.exit(0)
-
-
-def start_proxy(port, mode, flow_file_name):
-    """
-    Start Proxy
-    Capture / Intercept
-    """
-    mitm_proxy_opts = options.Options()
-    mitm_proxy_opts.keepserving = True
-    mitm_proxy_opts.listen_port = port
-    mitm_proxy_opts.cadir = settings.CA_DIR
-    if settings.UPSTREAM_PROXY:
-        mitm_proxy_opts.mode = settings.UPSTREAM_PROXY_CONF
-        mitm_proxy_opts.ssl_insecure = settings.UPSTREAM_PROXY_SSL_INSECURE
-    APPSERVER = ProxyHandler(mitm_proxy_opts, mode, flow_file_name)
-    APPSERVER.server = ProxyServer(ProxyConfig(mitm_proxy_opts))
-    # needed or not?
-    # proxy_server.addons.trigger("configure", mitm_proxy_opts.keys())
-    # proxy_server.addons.trigger("tick")
-    APPSERVER.run()
+    def signal_handler(self, *args, **kwargs):
+        try:
+            APPSERVER.shutdown()
+        except:
+            pass
+        try:
+            tornado.ioloop.IOLoop.instance().stop()
+        except:
+            pass
+        sys.exit(0)
 
 
-def run_fuzz_server(port):
-    """
-    Start Fuzz Server
-    Configure and Fuzz
-    """
-    print("Running Web GUI at *:%d" % port)
-    WEBSERVER = Application()
-    WEBSERVER.listen(port)
-    tornado.ioloop.IOLoop.current().start()
+    def start_proxy(self, port, mode, flow_file_name):
+        """
+        Start Proxy
+        Capture / Intercept
+        """
+        mitm_proxy_opts = options.Options()
+        mitm_proxy_opts.keepserving = True
+        mitm_proxy_opts.listen_port = port
+        mitm_proxy_opts.cadir = settings.CA_DIR
+        if settings.UPSTREAM_PROXY:
+            mitm_proxy_opts.mode = settings.UPSTREAM_PROXY_CONF
+            mitm_proxy_opts.ssl_insecure = settings.UPSTREAM_PROXY_SSL_INSECURE
+        APPSERVER = ProxyHandler(mitm_proxy_opts, mode, flow_file_name)
+        APPSERVER.server = ProxyServer(ProxyConfig(mitm_proxy_opts))
+        # needed or not?
+        # proxy_server.addons.trigger("configure", mitm_proxy_opts.keys())
+        # proxy_server.addons.trigger("tick")
+        APPSERVER.run()
 
+
+    def run_fuzz_server(self, port):
+        """
+        Start Fuzz Server
+        Configure and Fuzz
+        """
+        print("Running Web GUI at *:%d" % port)
+        WEBSERVER = Application()
+        WEBSERVER.listen(port)
+        tornado.ioloop.IOLoop.current().start()
+
+    def run_fuzz_cmdline(self, mode, project):
+        if project:
+            flow_file = get_flow_file(project)
+        else:
+            flow_file = get_flow_file("default")
+        fuzz_options = {}
+        fuzz_options["mode"] = mode
+        fuzz_options["include_scope"] = []
+        fuzz_options["exclude_scope"] = []
+        fuzz_options["exclude_url_match"] = "on"
+        fuzz_options["exclude_extensions"] = "on"
+        fuzz_options["exclude_response_code"] = "on"
+        fuzz_options["active_fuzzers"] = ["all"]
+        fuzz_options["flow_file"] = flow_file
+        fuzz_options["api_login"] = ""
+        fuzz_options["api_pin"] = ""
+        fuzz_options["api_register"] = ""
+        fuzz_options["write"] = ScanProgress.write
+        run_fuzzer(fuzz_options)
 
 def main():
 
@@ -89,31 +111,19 @@ def main():
     ARGS = PARSER.parse_args()
     if ARGS.mode:
         try:
-            signal.signal(signal.SIGTERM, signal_handler)
-            signal.signal(signal.SIGINT, signal_handler)
+            capfuzz = CapFuzz()
+            signal.signal(signal.SIGTERM, capfuzz.signal_handler)
+            signal.signal(signal.SIGINT, capfuzz.signal_handler)
             create_dir([settings.FLOWS_DIR, settings.LOGS_DIR])
             if ARGS.mode == "capture" or ARGS.mode == "intercept":
-                start_proxy(ARGS.port, ARGS.mode, ARGS.name)
+                capfuzz.start_proxy(ARGS.port, ARGS.mode, ARGS.name)
 
             elif ARGS.mode == "fuzz":
-                run_fuzz_server(ARGS.port)
+                capfuzz.run_fuzz_server(ARGS.port)
 
             elif ARGS.mode == "runfuzz":
-                if ARGS.name:
-                    FLOW_FILE = get_flow_file(ARGS.name)
-                else:
-                    FLOW_FILE = get_flow_file("default")
-                FUZZ_OPTS = {}
-                FUZZ_OPTS["mode"] = ARGS.mode
-                FUZZ_OPTS["include_scope"] = []
-                FUZZ_OPTS["exclude_scope"] = []
-                FUZZ_OPTS["exclude_url_match"] = "on"
-                FUZZ_OPTS["exclude_extensions"] = "on"
-                FUZZ_OPTS["exclude_response_code"] = "on"
-                FUZZ_OPTS["active_fuzzers"] = ["all"]
-                FUZZ_OPTS["flow_file"] = FLOW_FILE
-                FUZZ_OPTS["write"] = ScanProgress.write
-                run_fuzzer(FUZZ_OPTS)
+                capfuzz.run_fuzz_cmdline(ARGS.mode, ARGS.name)
+
             else:
                 PARSER.print_help()
         except (KeyboardInterrupt, RuntimeError) as e:
